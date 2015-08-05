e85cd74a5ed999652a20acaf52b44e6b08e1f855
Daniel McAssey <daniel.mcassey@glokon.com>
2015-08-05 15:56:09 +0100

    Fix naming conventions

--- src/GLOKON/CrowdAuth/CrowdAuthServiceProvider.php
+++ src/GLOKON/CrowdAuth/CrowdAuthServiceProvider.php
@@ -29,7 +29,7 @@ class CrowdAuthServiceProvider extends ServiceProvider {
      */
     public function boot()
     {
-        $this->package('glokon/laravel-crowd-auth', 'crowd-auth');
+        $this->package('glokon/crowd-auth', 'crowd-auth');
         \Auth::extend('crowd-auth', function() {
             return new \Illuminate\Auth\Guard(new \GLOKON\CrowdAuth\CrowdAuthUserProvider, \App::make('session.store'));
         });
